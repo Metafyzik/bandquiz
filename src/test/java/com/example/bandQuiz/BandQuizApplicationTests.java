a0accf8264228865b0fef138e2bda8bb9509545f
Igor Kuruc <krc13@email.cz>
2025-08-07 21:43:43 +0200

    Add test file

---
+++ src/test/java/com/example/bandQuiz/BandQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bandQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
